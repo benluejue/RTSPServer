6a6dd537011187815fc6eeed306e9de8c742774c
benluejue <1334146889@qq.com>
2023-05-11 12:36:24 +0800

    first commit

---
+++ autobuild.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# set -x调试选项，可以帮助我们识别处运行过程中出行的问题
+set -x
+
+rm -rf `pwd`/build/*
+# && 连接两个命令，只有第一个执行后，后面的才执行
+# 执行完毕后删除build下的东西
+cd `pwd`/build &&
+    cmake ../test &&
+    make &&
+    rm -rf *
